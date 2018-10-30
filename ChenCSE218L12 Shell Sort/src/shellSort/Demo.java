7a9e61e69ff82453b7622d551cb38aed9088acd0
Chen <chenb@sunysuffolk.edu>
2018-10-29 22:36:15 -0400

    Quick Sort and Shell Sort

---
+++ ChenCSE218L12 Shell Sort/src/shellSort/Demo.java
@@ -0,0 +1,24 @@
+package shellSort;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		int maxSize = 10;
+		ArraySh arr = new ArraySh(maxSize);
+		arr.insert(2);
+		arr.insert(9);
+		arr.insert(5);
+		arr.insert(4);
+		arr.insert(7);
+		arr.insert(0);
+		arr.insert(3);
+		arr.insert(6);
+		arr.insert(1);
+		arr.insert(8);
+		
+		arr.display();
+		arr.shellSort();
+		arr.display();
+	}
+
+}
